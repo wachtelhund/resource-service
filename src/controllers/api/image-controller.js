36c0cf45ea2f433be5a9226ce7b57f034dc25a49
Hampus Nilsson <hn222te@student.lnu.se>
2023-03-06 14:58:32 +0100

    Verify jwt

--- src/controllers/api/image-controller.js
+++ src/controllers/api/image-controller.js
@@ -1,14 +1,40 @@
 export class ImageController {
-  async getAll (req, res, next) {}
+  async getAll (req, res, next) {
+    console.log('getAll()')
+    res
+      .status(200)
+      .json({ message: 'getAll()' })
+  }
 
-  async postAll (req, res, next) {}
+  async postAll (req, res, next) {
+    console.log('postAll()')
+    res
+      .status(200)
+      .json({ message: 'postAll()' })
+  }
 
-  async getOne (req, res, next) {}
+  async getOne (req, res, next) {
+    console.log('getOne()')
+    res
+      .status(200)
+      .json({ message: 'getOne()' })
+  }
 
-  async putOne (req, res, next) {}
+  async putOne (req, res, next) {
+    console.log('putOne()')
+    res
+      .status(200)
+      .json({ message: 'putOne()' })
+  }
 
-  async patchOne (req, res, next) {}
+  async patchOne (req, res, next) {
+    console.log('patchOne()')
+    res
+      .status(200)
+      .json({ message: 'patchOne()' })
+  }
 
-  async deleteOne (req, res, next) {}
-
+  async deleteOne (req, res, next) {
+    console.log('deleteOne()')
+  }
 }
--- src/routes/api/v1/router.js
+++ src/routes/api/v1/router.js
@@ -6,12 +6,28 @@
  */
 import express from 'express'
 import { ImageController } from '../../../controllers/api/image-controller.js'
+import jwt from 'jsonwebtoken'
+import createError from 'http-errors'
 
 const controller = new ImageController()
 
 export const router = express.Router()
 
-router.get('/images', (req, res, next) => controller.getAll(req, res, next))
+const authenticate = (req, res, next) => {
+  try {
+    const type = req.headers.authorization.split(' ')[0]
+    const token = req.headers.authorization.split(' ')[1]
+    const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    if (!type === 'Bearer' || !payload) {
+      throw new Error()
+    }
+    next()
+  } catch (error) {
+    next(createError(401, 'Unauthorized'))
+  }
+}
+
+router.get('/images', authenticate, (req, res, next) => controller.getAll(req, res, next))
 router.post('/images', (req, res, next) => controller.postAll(req, res, next))
 router.get('/images/:id', (req, res, next) => controller.getOne(req, res, next))
 router.put('/images/:id', (req, res, next) => controller.putOne(req, res, next))

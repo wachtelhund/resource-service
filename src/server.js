2616fe11b92db848f73b31b03ef98ca1364a49c7
Hampus Nilsson <hn222te@student.lnu.se>
2023-03-06 10:15:04 +0100

    Add server

---
+++ src/server.js
@@ -0,0 +1,65 @@
+/**
+ * The starting point of the application.
+ *
+ * @author Hampus Nilsson
+ * @version 2.0.0
+ */
+
+import express from 'express'
+import helmet from 'helmet'
+import logger from 'morgan'
+import { router } from './routes/router.js'
+import { connectDB } from './config/mongoose.js'
+
+try {
+  await connectDB()
+
+  const app = express()
+
+  app.use(helmet())
+
+  app.use(logger('dev'))
+
+  app.use(express.json({ limit: '500kb' }))
+
+  app.use(express.urlencoded({ extended: false }))
+
+  app.use('/', router)
+
+  app.use(function (err, req, res, next) {
+    err.status = err.status || 500
+
+    if (req.app.get('env') !== 'development') {
+      return res
+        .status(err.status)
+        .json({
+          status: err.status,
+          message: err.message
+        })
+    }
+
+    return res
+      .status(err.status)
+      .json({
+        status: err.status,
+        message: err.message,
+        cause: err.cause
+          ? {
+              status: err.cause.status,
+              message: err.cause.message,
+              stack: err.cause.stack
+            }
+          : null,
+        stack: err.stack
+      })
+  })
+
+  // Starts the HTTP server listening for connections.
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running at http://localhost:${process.env.PORT}`)
+    console.log('Press Ctrl-C to terminate...')
+  })
+} catch (err) {
+  console.error(err)
+  process.exitCode = 1
+}
